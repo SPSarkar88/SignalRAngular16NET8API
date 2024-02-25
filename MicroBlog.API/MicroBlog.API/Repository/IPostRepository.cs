bd9234414b5bbdadee07e4936e3b60d866962b0e
Subhro Prokash Sarkar <78630512+SPSarkar88@users.noreply.github.com>
2024-02-25 13:22:07 +0530

    Added Post Repositoty

--- MicroBlog.API/MicroBlog.API/Repository/IPostRepository.cs
+++ MicroBlog.API/MicroBlog.API/Repository/IPostRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace MicroBlog.API.Repository
+﻿using MicroBlog.API.Models;
+
+namespace MicroBlog.API.Repository
 {
     public interface IPostRepository
     {
+        Task<Post> AddPost(Post post);
+        Task<bool> DeletePost(Guid id, string uid);
+        Task<Post?> GetPost(Guid id, string uid);
+        Task<IEnumerable<Post>> GetPosts();
+        Task<bool> UpdatePost(Post post, Guid id, string uid);
     }
 }
--- MicroBlog.API/MicroBlog.API/Repository/PostRepository.cs
+++ MicroBlog.API/MicroBlog.API/Repository/PostRepository.cs
@@ -1,6 +1,63 @@
-﻿namespace MicroBlog.API.Repository
+﻿using MicroBlog.API.AppDbContext;
+using MicroBlog.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MicroBlog.API.Repository
 {
     public class PostRepository : IPostRepository
     {
+        private readonly BlogDbContext _context;
+        public PostRepository(BlogDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Post> AddPost(Post post)
+        {
+            await _context.Posts.AddAsync(post);
+            await _context.SaveChangesAsync();
+            return post;
+        }
+
+        private async Task<Post?> GetPostWithTracking(Guid id, string uid)
+        {
+            return await _context.Posts
+                .Where(x => x.Id == id)
+                .Where(x => x.Uid == uid)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<Post?> GetPost(Guid id, string uid)
+        {
+            return await _context.Posts
+                .AsNoTracking()
+                .Include(p => p.ImageId)
+                .Where(x => x.Id == id)
+                .Where(x => x.Uid == uid)
+                .FirstOrDefaultAsync();
+        }
+        public async Task<IEnumerable<Post>> GetPosts()
+        {
+            return await _context.Posts
+                .AsNoTracking()
+                .Include(p => p.ImageId)
+                .ToListAsync();
+        }
+
+        public async Task<bool> UpdatePost(Post post, Guid id, string uid)
+        {
+            _context.Posts.Update(post);
+            var isUpdateSucess = await _context.SaveChangesAsync();
+            return isUpdateSucess > 0;
+        }
+
+        public async Task<bool> DeletePost(Guid id, string uid)
+        {
+            var postToDelete = await GetPostWithTracking(id, uid);
+            if (postToDelete == null)
+                return false;
+            _context.Posts.Remove(postToDelete);
+            var isDeleteSucess = await _context.SaveChangesAsync();
+            return isDeleteSucess > 0;
+        }
     }
 }
