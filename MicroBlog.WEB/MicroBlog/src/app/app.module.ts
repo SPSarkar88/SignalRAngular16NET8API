677cdd7baf95b69b6be54bf35b4428d4e9b2e2e5
Subhro Prokash Sarkar <78630512+SPSarkar88@users.noreply.github.com>
2024-02-25 20:28:37 +0530

    Added jumbotron

--- MicroBlog.WEB/MicroBlog/src/app/app.module.ts
+++ MicroBlog.WEB/MicroBlog/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PostListComponent } from './component/post-list/post-list.component';
 import { AppNavbarComponent } from './layout/app-navbar/app-navbar.component';
+import { JumbotronComponent } from './layout/jumbotron/jumbotron.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostListComponent,
     AppNavbarComponent,
+    JumbotronComponent
   ],
   imports: [
     BrowserModule
