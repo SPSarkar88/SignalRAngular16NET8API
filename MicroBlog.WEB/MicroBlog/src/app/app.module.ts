0315d44b4846f1223f1d3408a39a2fec6d9d58d4
Subhro Prokash Sarkar <78630512+SPSarkar88@users.noreply.github.com>
2024-02-25 20:09:23 +0530

    Added post list component

--- MicroBlog.WEB/MicroBlog/src/app/app.module.ts
+++ MicroBlog.WEB/MicroBlog/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PostListComponent } from './component/post-list/post-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule
