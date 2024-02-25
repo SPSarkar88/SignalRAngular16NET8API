1223974ff01efbf2c4380b32555c3d336082f9b4
Subhro Prokash Sarkar <78630512+SPSarkar88@users.noreply.github.com>
2024-02-25 22:02:00 +0530

    added homepage link

--- MicroBlog.WEB/MicroBlog/src/app/app-routing.module.ts
+++ MicroBlog.WEB/MicroBlog/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {PostListComponent} from "./component/post-list/post-list.component";
+import {JumbotronComponent} from "./layout/jumbotron/jumbotron.component";
 
 const routes: Routes = [
   { path: 'posts', component: PostListComponent },
+  { path: 'home', component: JumbotronComponent },
   // { path: 'post/:id/:uid', component: PostComponent  },
   // { path: 'post-create', component: PostCreateComponent, },
   // { path: 'post-update', component: PostUpdateComponent, },
-  // { path: '',   redirectTo: '/videogame-list', pathMatch: 'full' }
+   { path: '',   redirectTo: '/home', pathMatch: 'full' }
 ];
 
 @NgModule({
