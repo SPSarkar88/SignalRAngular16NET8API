917eb9088c08d578e6163546ed3bb4bbe32a953f
Subhro Prokash Sarkar <78630512+SPSarkar88@users.noreply.github.com>
2024-02-25 20:14:18 +0530

    Added app navbar component

--- MicroBlog.WEB/MicroBlog/src/app/app.module.ts
+++ MicroBlog.WEB/MicroBlog/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PostListComponent } from './component/post-list/post-list.component';
+import { AppNavbarComponent } from './layout/app-navbar/app-navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostListComponent
+    PostListComponent,
+    AppNavbarComponent
   ],
   imports: [
     BrowserModule
