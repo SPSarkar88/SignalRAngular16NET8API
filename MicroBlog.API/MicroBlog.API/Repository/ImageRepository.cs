1be16b71cbea8f2ea9c20f87eab6a00c3b8fbe92
Subhro Prokash Sarkar <78630512+SPSarkar88@users.noreply.github.com>
2024-02-26 23:40:26 +0530

    removed

--- MicroBlog.API/MicroBlog.API/Repository/ImageRepository.cs
+++ MicroBlog.API/MicroBlog.API/Repository/ImageRepository.cs
@@ -3,30 +3,30 @@ using MicroBlog.API.Models;
 
 namespace MicroBlog.API.Repository
 {
-    public class ImageRepository
-    {
-        private readonly BlogDbContext _context;
-        public ImageRepository(BlogDbContext context)
-        {
-            _context = context;
-        }
+    //public class ImageRepository
+    //{
+    //    private readonly BlogDbContext _context;
+    //    public ImageRepository(BlogDbContext context)
+    //    {
+    //        _context = context;
+    //    }
 
-        public async Task AddImageToPost(string id)
-        {
-            var post = await _context.Posts.FindAsync(id);
-            if(post == null)
-            {
-                throw new Exception("Post not found.");
-            }
-            var image = new Image
-            {
-                Id = Guid.NewGuid(),
-                PostId = Guid.Parse(id.ToString())
-            };
-            post.Image = image;
-            await _context.Posts.AddAsync(post);
-            await _context.SaveChangesAsync();
-        }
+    //    public async Task AddImageToPost(string id)
+    //    {
+    //        var post = await _context.Posts.FindAsync(id);
+    //        if(post == null)
+    //        {
+    //            throw new Exception("Post not found.");
+    //        }
+    //        var image = new Image
+    //        {
+    //            Id = Guid.NewGuid(),
+    //            PostId = Guid.Parse(id.ToString())
+    //        };
+    //        post.Image = image;
+    //        await _context.Posts.AddAsync(post);
+    //        await _context.SaveChangesAsync();
+    //    }
 
-    }
+    //}
 }
