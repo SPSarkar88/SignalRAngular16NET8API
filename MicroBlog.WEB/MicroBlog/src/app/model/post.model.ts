6ebeeea9657be13317bbf919e35427ac5b7844f0
Subhro Prokash Sarkar <78630512+SPSarkar88@users.noreply.github.com>
2024-02-26 19:25:01 +0530

    Converted into interface

--- MicroBlog.WEB/MicroBlog/src/app/model/post.model.ts
+++ MicroBlog.WEB/MicroBlog/src/app/model/post.model.ts
@@ -1,10 +1,5 @@
-export class Post {
+export interface Post {
     postContent: string;
     created_at: Date;
     updated_at: Date;
-    constructor(postContent: string, created_at: Date, updated_at: Date) {
-        this.postContent = postContent;
-        this.created_at = created_at;
-        this.updated_at = updated_at;
-    }
 }
